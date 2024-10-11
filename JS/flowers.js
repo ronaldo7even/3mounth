40db32993117d32a091600ae948aff85ebf5dff9
ronaldo7even <sbittencourt7891@gmail.com>
2024-10-10 21:56:26 -0300

    alteraçoes

--- JS/flowers.js
+++ JS/flowers.js
@@ -5,4 +5,16 @@ onload = () => {
     }, 1000);
   };
 
+  onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+  
+      // Mostrar botão para ver fotos após a animação das flores
+      const btnContainer = document.querySelector(".button-container");
+      btnContainer.style.display = "block";
+    }, 4000); // Exibir após 4 segundos (ajustar se necessário)
+  };
+  
+
 
